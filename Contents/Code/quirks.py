b4d1b6609a36629c948c05614dd195e5bc9488da
iquadtree <iquadtree@users.noreply.github.com>
2025-04-06 07:22:27 +0700

    Pylint fix: module methods overrides (according to Plex Python flavor)

--- Contents/Code/quirks.py
+++ Contents/Code/quirks.py
@@ -5,14 +5,14 @@
     License: MIT
 """
 
-def all(iterable):
+def all(iterable): # pylint: disable=W0622
     """Return True if all elements of the iterable are true (or if the iterable is empty)"""
     for element in iterable:
         if not element:
             return False
     return True
 
-def any(iterable):
+def any(iterable): # pylint: disable=W0622
     """Return True if any element of the iterable is true. If the iterable is empty return False"""
     for element in iterable:
         if element:
